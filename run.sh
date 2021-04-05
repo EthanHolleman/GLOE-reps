caf7cfa2734b4ba165563a785ff5a5fa2e804b78
Your Name <etholleman@ucdavis.edu>
2021-04-04 21:33:53 -0700

    Use mamba

--- run.sh
+++ run.sh
@@ -6,4 +6,5 @@ snakemake -j 14 -s Snakefile --cluster-config cluster.yml \
 --cluster "sbatch -p {cluster.partition} -t {cluster.time} -N {cluster.nodes} \
 -n {cluster.cpus} --mem {cluster.mem} -J {cluster.name} -o {cluster.output} \
 -e {cluster.output} --mail-type ALL --mail-user {cluster.email}" \
+--conda-frontend=mamba \
 --latency-wait 120 --verbose --use-conda -p -k $@
