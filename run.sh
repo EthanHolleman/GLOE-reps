7c2633fe1a261edb525808023e2b6393558291d7
Your Name <etholleman@ucdavis.edu>
2021-04-07 21:19:27 -0700

    and --rerun-incomplete flag

--- run.sh
+++ run.sh
@@ -7,4 +7,5 @@ snakemake -j 14 -s Snakefile --cluster-config cluster.yml \
 -n {cluster.cpus} --mem {cluster.mem} -J {cluster.name} -o {cluster.output} \
 -e {cluster.output} --mail-type ALL --mail-user {cluster.email}" \
 --conda-frontend=mamba \
---latency-wait 120 --verbose --use-conda -p -k $@
+--latency-wait 120 --verbose --use-conda --rerun-incomplete \
+-p -k $@
